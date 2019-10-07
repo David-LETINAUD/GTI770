9b55004d010da637aa291239ebc8b3caaad33344
Thomas Lioret <thomas@mactom.local>
2019-10-07 11:43:20 -0400

    modif bayes

--- GTI770_Laboratoire2_-_BLEA14058906_LETD05129708_LIOT20069605/zoo_BAYES.py
+++ GTI770_Laboratoire2_-_BLEA14058906_LETD05129708_LIOT20069605/zoo_BAYES.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python3                                                                                                      
+`#! /usr/bin/env python3                                                                                                      
 # -*- coding: utf-8 -*-      
 
 # Imports                                                                                                                                                 
@@ -14,8 +14,7 @@ from sklearn import datasets
 iris = datasets.load_iris()
 
 gnb = GaussianNB()
-y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
-print("Number of mislabeled points out of a total %d points : %d"% (iris.data.shape[0],(iris.target != y_pred).sum()))
+y_pred = gnb.fit(iris.data, iris.target)
 
 
 #scale data + multinomial bayes                                                                                                                           
