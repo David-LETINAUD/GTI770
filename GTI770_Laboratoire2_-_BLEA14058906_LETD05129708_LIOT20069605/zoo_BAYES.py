3a0b07ead4930d16ab8ca7c3d57e106a3d15ea88
Thomas Lioret <thomas@mactom.local>
2019-10-15 11:07:03 -0400

    fonction + plot ok pour les 3 cas (bayes)

--- GTI770_Laboratoire2_-_BLEA14058906_LETD05129708_LIOT20069605/zoo_BAYES.py
+++ GTI770_Laboratoire2_-_BLEA14058906_LETD05129708_LIOT20069605/zoo_BAYES.py
@@ -1,32 +1,39 @@
 #! /usr/bin/env python3                                                                                                      
-# -*- coding: utf-8 -*-                                                                                                                                                       
+# -*- coding: utf-8 -*- 
 
-"""                                                                                                                                                                                  
-Course :                                                                                                                                                                             
-GTI770 — Systèmes intelligents et apprentissage machine                                                                                                                              
-                                                                                                                                                                                     
-Project :                                                                                                                                                                            
-Lab # 2 — Arbre de décision, Bayes Naïf et KNN                                                                                                                                       
-                                                                                                                                                                                     
-Students :                                                                                                                                                                           
-Alexendre Bleau — BLEA14058906                                                                                                                                                       
-David Létinaud  — LETD05129708                                                                                                                                                       
-Thomas Lioret   — LIOT20069605                                                                                                                                                       
-                                                                                                                                                                                     
-Group :                                                                                                                                                                              
-GTI770-A19-01                                                                                                                                                                        
+"""
+Course :
+
+GTI770 — Systèmes intelligents et apprentissage machine
+
+Project : 
+
+Lab # 2 — Arbre de décision, Bayes Naïf et KNN
+
+Students : 
+
+Alexendre Bleau — BLEA14058906 
+David Létinaud  — LETD05129708
+Thomas Lioret   — LIOT20069605
+
+Group :
+
+GTI770-A19-01
+
 """
 
 import csv
 from zoo_tree import zoo_tree
 
 
-########################################   Initialisations   ########################################                                                                                
+########################################   Initialisations   ########################################                                                                               
+
 dataset_path = "/Users/thomas/Desktop/COURS_ETS/gti770/data/csv/galaxy/galaxy_feature_vectors.csv"
 
-# Nombre d'images total du dataset (training + testing)                                                                                                                              
+# Nombre d'images total du dataset (training + testing)
 nb_img = 100
-# Pourcentage de données utilisées pour l'entrainement                                                                                                                               
+
+# Pourcentage de données utilisées pour l'entrainement 
 ratio_train = 0.7
 
 
@@ -74,15 +81,22 @@ from itertools import product
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, train_size=ratio_train, random_state=1) # 70% training and 30% test
 
 #sans prétraitement + Gaussian
-# Création d'un arbre de décision                                                                                                                                                
-clf = GaussianNB(priors=None, var_smoothing=1e-09) #Priors : probabilité des classes (on les considères égales on y touche pas). 
-clf = clf.fit(X_train,Y_train)
-# Prévoir la réponse pour l'ensemble de données de test                                                                                                                          
-Y_pred = clf.predict(X_test)
-acc_ = metrics.accuracy_score(Y_test, Y_pred)
-print("Accuracy gaussian sans prétraitement:",acc_)
-score_ = metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None)
-print("f1_score gaussian sans prétraitement:", score_)
+
+def bayes_gaussian_noProcess(X_train,X_test,Y_train,Y_test):
+    """
+    """
+    clf = GaussianNB(priors=None, var_smoothing=1e-09) #Priors : probabilité des classes (on les considère égales on y touche pas).
+    clf = clf.fit(X_train,Y_train)
+    # Prévoir la réponse pour l'ensemble de données de test
+    Y_pred = clf.predict(X_test)
+    acc_ = metrics.accuracy_score(Y_test, Y_pred)
+    score_ = metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None)
+    return([acc_,score_])
+
+print("yoyoyoyoyooyoyoy")
+print(bayes_gaussian_noProcess(X_train,X_test,Y_train,Y_test))
+print("yoyoyoyoy")
+
 
 
 #Variation et plot des hypers paramètres
@@ -111,7 +125,7 @@ plt.legend()
 
 
 
-print("penser à stocker le max qque part")
+
 print("=========================================================================================")
 
 
@@ -129,6 +143,26 @@ print("Accuracy multinomial bayes avec scale data:",acc_)
 score_ = metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None)
 print("f1_score multinomial bayes avec scale data:", score_)
 
+
+def bayes_mutltinomial_scaleData(X_train,X_test,Y_train,Y_test,scale):
+    """
+    """
+    
+    scaler = MinMaxScaler(feature_range = (0, scale), copy=True)
+    X_train_scale = scaler.fit_transform(X_train) #On scale les data d'entrainement
+    X_test_scale = scaler.fit_transform(X_test) #On scale les data de test
+    clf = MultinomialNB()
+    clf = clf.fit(X_train_scale,Y_train)
+    Y_pred = clf.predict(X_test_scale)
+    acc_ = metrics.accuracy_score(Y_test,Y_pred)
+    score_ = metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None)
+    
+    return([acc_,score_])
+
+
+print("yoyoyoyo")
+print(bayes_mutltinomial_scaleData(X_train,X_test,Y_train,Y_test,1))
+print("coucou")
 
 list_scaler = [(0,i) for i in np.linspace(0.2,1,10)]
 x_plot = []
@@ -154,9 +188,9 @@ ax2.plot(x_plot,f1_plot,"xb",label = "f1_score")
 ax2.set(xlabel='feature_range', ylabel='f1_score et accuracy',title='f1_score et accuracy en fonction de feature_range')
 ax2.grid()
 plt.legend()
-plt.show()
 
-print("penser à stocker le max qque part")
+
+
 print("=========================================================================================")
                                                                                                  
 
@@ -174,18 +208,15 @@ print("Accuracy multinomial bayes avec K-Bins discretization:",acc_)
 score_ = metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None)
 print("f1_score multinomial bayes avec K-Bins discretization:", score_)
 print("ok")
+print("=========================================================================================")
 
 
 
-#list_nbins = [i for i in range(5,6)]
-list_encode = ["onehot", "onehot-dense", "ordinal"]
-list_strategy = ["uniform", "quantile", "kmeans"]
-x_plot = []
-acc_plot = []
-f1_plot = []
+def bayes_multinomial_kbinDiscetization(X_train,X_test,Y_train,Y_test,nb_bins):
+    """
+    """
 
-for enc,strat in product (list_encode,list_strategy):
-    pre_proc = preprocessing.KBinsDiscretizer(n_bins =10, encode = enc, strategy = strat).fit(X)
+    pre_proc = preprocessing.KBinsDiscretizer(n_bins = nb_bins, encode='ordinal', strategy='uniform').fit(X) #Jouer avec les hypers paramètres
     X_train_pp = pre_proc.transform(X_train) #preprocessing des data
     X_test_pp = pre_proc.transform(X_test)
     clf = MultinomialNB()
@@ -193,3 +224,39 @@ for enc,strat in product (list_encode,list_strategy):
     Y_pred = clf.predict(X_test_pp)
     acc_ = metrics.accuracy_score(Y_test,Y_pred)
     score_ = metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None)
+    return([acc_,score_])
+
+
+print("coucou")
+print(bayes_multinomial_kbinDiscetization(X_train,X_test,Y_train,Y_test,10))
+print("coucou")
+
+list_nbins = np.arange(3,10,1)
+x_plot = []
+acc_plot = []
+f1_plot = []
+
+for bin in list_nbins:
+    pre_proc = preprocessing.KBinsDiscretizer(n_bins = bin, encode = "ordinal", strategy = "uniform").fit(X)
+    X_train_pp = pre_proc.transform(X_train) #preprocessing des data
+    X_test_pp = pre_proc.transform(X_test)
+    clf = MultinomialNB()
+    clf = clf.fit(X_train_pp,Y_train)
+    Y_pred = clf.predict(X_test_pp)
+    acc_ = metrics.accuracy_score(Y_test,Y_pred)
+    score_ = metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None)
+    x_plot.append(bin)
+    acc_plot.append(acc_)
+    f1_plot.append(score_)
+
+
+#print(acc_plot)
+#print(f1_plot)
+
+fig, ax3 = plt.subplots()
+ax3.plot(x_plot,acc_plot,"or",label = "accuracy")
+ax3.plot(x_plot,f1_plot,"xb",label = "f1_score")
+ax3.set(xlabel='nombre de bins', ylabel='f1_score et accuracy',title='f1_score et accuracy en fonction du nombre de bins')
+ax3.grid()
+plt.legend()
+plt.show()
