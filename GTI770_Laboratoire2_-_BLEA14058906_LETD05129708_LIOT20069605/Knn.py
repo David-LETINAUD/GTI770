63fb1b8176598fd71bb45f207a2b24035442fa54
David-LETINAUD <51359189+David-LETINAUD@users.noreply.github.com>
2019-10-21 23:16:47 -0400

    correction rapide

--- GTI770_Laboratoire2_-_BLEA14058906_LETD05129708_LIOT20069605/Knn.py
+++ GTI770_Laboratoire2_-_BLEA14058906_LETD05129708_LIOT20069605/Knn.py
@@ -23,49 +23,42 @@ from sklearn.metrics import confusion_matrix
 
 from sklearn.preprocessing import MinMaxScaler
 
-########################################   Initialisations   ########################################
 
-# image_path = "C:/Users/David/Desktop/GTI770/data/data/images/"
-# image_path = '/Users/thomas/Desktop/COURS_ETS/gti770/data/images/'
-# dataset_path = "C:/Users/David/Desktop/GTI770/data/data/csv/galaxy/galaxy_label_data_set.csv"
-# dataset_path = '/Users/thomas/Desktop/COURS_ETS/gti770/data/csv/galaxy/galaxy_label_data_set.csv'
-#dataset_path = "/home/alex/Desktop/GTI770-tp2/csv/galaxy/galaxy_feature_vectors.csv"
-#image_path = "/home/alex/Desktop/GTI770-tp2/csv/images/"
- """
-    Fonction qui permet de calculer la precision de notre matrice de confusion
-    
-    input :
-        matrice (nparray) : matrice de confusion
-        
-    output : 
-        Precision(int)    : suivant le calculte (Tp+Fp)/(Tp+fp+fn+tn)
-    
-    """
+ def accknn(matrice):
+  """
+  Fonction qui permet de calculer la precision de notre matrice de confusion
 
-def accknn(matrice):
+  input :
+      matrice (nparray) : matrice de confusion
+
+  output : 
+      Precision(int)    : suivant le calcul (Tp+Fp)/(Tp+fp+fn+tn)
+
+  """
     deno = matrice[0][0] + matrice[1][1]
 
     nume = matrice[0][0] + matrice[0][1] + matrice[1][0] + matrice[1][1]
 
     acc = (float(deno) / float(nume))
     return acc
-"""
-    Fonction qui permet de calculer la precision et la valeur du F1_score selon un parametre K voisin
-    
-    input :
-        XTrain (nparray):  tableau des tableau des features destinées à l'entrainement.
-        Xtest  (nparray):  tableau des features à tester aux tests.
-        Ytrain (nparray):  tableau des étiquettes associées aux valeurs d'entrainement.
-        Ytest  (nparray):  tableau des étiquettes pour les valeurs de test.
-        k (int):           valeur numerique K pour determiner le nombre de voisin interoge
-        
-    output : 
-        Acc (int)       : precision selon la valeur k 
-        score_(int)     : F1_score selon la valeur k
-    
-    """
+
 
 def KNN(Xtrain, Xtest, Ytrain, Ytest, k):
+ """
+ Fonction qui permet de calculer la precision et la valeur du F1_score selon un parametre K voisin
+
+ input :
+     XTrain (nparray):  tableau des features destiné à l'entrainement.
+     Xtest  (nparray):  tableau des features destiné aux tests.
+     Ytrain (nparray):  tableau des étiquettes associée aux valeurs d'entrainement.
+     Ytest  (nparray):  tableau des étiquettes pour les valeurs de test.
+     k (int):           valeur numerique K pour determiner le nombre de voisin interogé
+
+ output : 
+     Acc (int)       : précision selon la valeur k 
+     score_(int)     : F1_score selon la valeur k
+
+ """
     scaler = MinMaxScaler(feature_range=(0, 1), copy=True)  # scale des data entre 0 et 1 par défaut.
     X_train_scale = scaler.fit_transform(Xtrain)  # On scale les data d'entrainement
     X_test_scale = scaler.fit_transform(Xtest)  # On scale les data de test
