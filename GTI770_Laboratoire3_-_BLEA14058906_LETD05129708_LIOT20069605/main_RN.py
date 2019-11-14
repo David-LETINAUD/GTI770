b6b0a8abc6a2e8f3d80a51b240ad273023e7293b
David-LETINAUD <david.letinaud@gmail.com>
2019-11-14 10:28:42 -0500

    main RN run all

--- GTI770_Laboratoire3_-_BLEA14058906_LETD05129708_LIOT20069605/main_RN.py
+++ GTI770_Laboratoire3_-_BLEA14058906_LETD05129708_LIOT20069605/main_RN.py
@@ -17,13 +17,12 @@ Group :
 GTI770-A19-01
 """
 
+# Initialisation
 from RN_model import *
 from functions import get_data , plot_perf, plot_delay
 import time
 import matplotlib.pyplot as plt
-
-
-from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.callbacks import TensorBoard # Pour affichage sur tensorboard
 import shutil
 
 X_train, X_test, Y_train, Y_test = get_data()
@@ -35,35 +34,198 @@ batch_size = 100
 
 dropout = 0.5
 
+# Pour affichage
+sub_title = ['loss','acc','f1','val_loss','val_acc', 'val_f1']
+x_lab = "epochs"
 
+################################## Nombres de couches cachees
 training_delay_RN = []
 predicting_delay_RN = []
 history_obj = []
-
+cpt = 0
 best_accuracy_RN = 0
-# Faire 1 test à la fois ou réinitialiser les 3 lists
 
-################################## Nombres de couches cachees
 layer_sizes_range = [[100],[100, 100, 2],[100, 100, 100, 100, 100, 2]]
 
-try:
+# Suppression de la dernière étude d'hyperparamètre
+
+try:      
     shutil.rmtree('./logs')
 except:
     print("nothing to delete")
-    
+# Callbacks pour affichage des performances dans tensorflow : 1 callback pour chaque hyperparamètre
 tensorboard_callback = []
 for i in range(3):
-    tensorboard_callback.append(TensorBoard(log_dir="logs\{}".format(i)))#time.time())))
+    tensorboard_callback.append(TensorBoard(log_dir="logs\{}".format(i)))
 # Par invité de commande : 
 # tensorboard --logdir="./logs" --port 6006
 cpt = 0
 for layer_s in layer_sizes_range:
     model = RN_model(layer_s, dropout, learning_rate)
+    #### Apprentissage                                                                                                                                                               
+    start = time.time()                                                                                                                   
+    hist_obj = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs, validation_data=(X_test, Y_test), callbacks = [tensorboard_callback[cpt]]) 
+
+    end = time.time()
+    training_delay_RN.append(end - start)
+
+    history_obj.append( list(hist_obj.history.values()))
+
+    #### Prédiction                                                                                                                                                                  
+    start = time.time()
+
+    Y_pred = np.where(model.predict(X_test) > 0.5, 1, 0)
+
+    end = time.time()
+    predicting_delay_RN.append(end - start)
+    cpt+=1
+
+# Mise en forme des données pour l'affichage
+ho = np.array(history_obj)
+ho = ho.transpose(1,2,0)
+
+leg = [str(i) for i in layer_sizes_range]                                                                                                                                              
+
+titre = "RN : HyperParam = number of layer"                                                                                                                                         
+
+plot_perf(ho, leg, titre ,sub_title)
+plot_delay(training_delay_RN,predicting_delay_RN,titre)
+
+################################## Nombres de perceptrons
+training_delay_RN = []
+predicting_delay_RN = []
+history_obj = []
+
+best_accuracy_RN = 0
+
+nb_perceptrons_range = [[5, 4, 4],[100, 100, 2],[500, 500, 500]]                                                                                                                      
+
+# Suppression de la dernière étude d'hyperparamètre
+try:    
+    shutil.rmtree('./logs')
+except:
+    print("nothing to delete")
+# Callbacks pour affichage des performances dans tensorflow : 1 callback pour chaque hyperparamètre
+tensorboard_callback = []
+for i in range(3):
+    tensorboard_callback.append(TensorBoard(log_dir="logs\{}".format(i)))
+# Par invité de commande : 
+# tensorboard --logdir="./logs" --port 6006
+cpt = 0
+for nb_perceptrons in nb_perceptrons_range:                                                                                                                                                  
+    model = RN_model(nb_perceptrons, dropout, learning_rate)                                                                                                                              
+    #### Apprentissage                                                                                                                                                             
+    start = time.time()                                                                                                                                                            
+    hist_obj = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs, validation_data=(X_test, Y_test), callbacks = [tensorboard_callback[cpt]])                                                             
+
+    end = time.time()                                                                                                                                                              
+    training_delay_RN.append(end - start)                                                                                                                                          
+
+    history_obj.append( list(hist_obj.history.values()))
+
+    #### Prédiction                                                                                                                                                                
+    start = time.time()                                                                                                                                                            
+
+    Y_pred = np.where(model.predict(X_test) > 0.5, 1, 0)                                                                                                                           
+
+    end = time.time()                                                                                                                                                              
+    predicting_delay_RN.append(end - start)    
+    cpt+=1   
+
+# Mise en forme des données pour l'affichage final
+ho = np.array(history_obj)
+ho = ho.transpose(1,2,0)
+
+leg = [str(i) for i in nb_perceptrons_range]  
+
+titre = "RN : HyperParam = layer size"                                                                                                                                          
+
+plot_perf(ho, leg, titre ,sub_title)
+plot_delay(training_delay_RN,predicting_delay_RN,titre)
+
+################################## Nombres d'iterations
+training_delay_RN = []
+predicting_delay_RN = []
+history_obj = []
+cpt = 0
+best_accuracy_RN = 0
+
+epochs_range = [30,60, 5000]                                                                                                                                            
+max_ep = max(epochs_range) 
+
+# Suppression de la dernière étude d'hyperparamètre
+try:
+    shutil.rmtree('./logs')
+except:
+    print("nothing to delete")
+# Callbacks pour affichage des performances dans tensorflow : 1 callback pour chaque hyperparamètre
+tensorboard_callback = []
+for i in range(3):
+    tensorboard_callback.append(TensorBoard(log_dir="logs\{}".format(i)))
+# Par invité de commande : 
+# tensorboard --logdir="./logs" --port 6006
+cpt = 0
+for ep in epochs_range:                                                                                                                                                            
+    model = RN_model(layer_sizes, dropout, learning_rate)                                                                                                                          
+    #### Apprentissage                                                                                                                                                             
+    start = time.time()                                                                                                                                                            
+    #model.fit(X_train, Y_train, batch_size = 100, epochs = 60)                                                                                                                    
+    hist_obj = model.fit(X_train, Y_train, batch_size = batch_size, epochs = ep, validation_data=(X_test, Y_test), callbacks = [tensorboard_callback[cpt]])                                                                  
+
+    end = time.time()                                                                                                                                                              
+    training_delay_RN.append(end - start)                                                                                                                                          
+
+    ho_tmp = list(hist_obj.history.values())                                                                                                                                       
+    ho_tmp = [i + [np.nan for _ in range(max_ep-ep)] for i in ho_tmp ]                                                                                                             
+    history_obj.append(ho_tmp)
+    #### Prédiction                                                                                                                                                                
+    start = time.time()                                                                                                                                                            
+
+    Y_pred = np.where(model.predict(X_test) > 0.5, 1, 0)                                                                                                                           
+
+    end = time.time()                                                                                                                                                              
+    predicting_delay_RN.append(end - start) 
+    cpt+=1
+
+# Mise en forme des données pour l'affichage final
+ho = np.array(history_obj)
+ho = ho.transpose(1,2,0)
+
+leg = [str(i) for i in epochs_range]                                                                                                                                                
+                                                                                                                                       
+titre = "RN : HyperParam = number of epochs"                                                                                                                                          
+
+plot_perf(ho, leg, titre ,sub_title)
+plot_delay(training_delay_RN,predicting_delay_RN,titre)
+
+################################## Learning rate 
+training_delay_RN = []
+predicting_delay_RN = []
+history_obj = []
+cpt = 0
+best_accuracy_RN = 0
+
+l_rate_range = [0.00001,0.0005,0.01]
+
+# Suppression de la dernière étude d'hyperparamètre
+try:
+    shutil.rmtree('./logs')
+except:
+    print("nothing to delete")
+
+# Callbacks pour affichage des performances dans tensorflow : 1 callback pour chaque hyperparamètre
+tensorboard_callback = []
+for i in range(3):
+    tensorboard_callback.append(TensorBoard(log_dir="logs\{}".format(i)))
+# Par invité de commande : 
+# tensorboard --logdir="./logs" --port 6006
+cpt = 0
+for l_rate in l_rate_range:
+    model = RN_model(layer_sizes, dropout, l_rate)
     #### Apprentissage
     start = time.time()
     #model.fit(X_train, Y_train, batch_size = 100, epochs = 60)
     hist_obj = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs, validation_data=(X_test, Y_test), callbacks = [tensorboard_callback[cpt]])
-    
     end = time.time()
     training_delay_RN.append(end - start)
 
@@ -71,139 +233,19 @@ for layer_s in layer_sizes_range:
 
     #### Prédiction
     start = time.time()
-    
+
     Y_pred = np.where(model.predict(X_test) > 0.5, 1, 0)
 
     end = time.time()
     predicting_delay_RN.append(end - start)
     cpt+=1
-################################## Nombres de perceptrons
-# layer_sizes_range = [[5, 4, 4],[100, 100, 2],[500, 500, 500]]
-
-# try:
-#     shutil.rmtree('./logs')
-# except:
-#     print("nothing to delete")
-    
-# tensorboard_callback = []
-
-
-# for i in range(3):
-#     tensorboard_callback.append(TensorBoard(log_dir="logs\{}".format(i)))
-# # Par invité de commande : 
-# # tensorboard --logdir="./logs" --port 6006
-# cpt = 0
-# for layer_s in layer_sizes_range:
-#     model = RN_model(layer_s, dropout, learning_rate)
-#     #### Apprentissage
-#     start = time.time()
-#     #model.fit(X_train, Y_train, batch_size = 100, epochs = 60)
-#     hist_obj = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs, validation_data=(X_test, Y_test), callbacks = [tensorboard_callback[cpt]])
-
-#     end = time.time()
-#     training_delay_RN.append(end - start)
-
-#     history_obj.append( list(hist_obj.history.values()))
-
-#     #### Prédiction
-#     start = time.time()
-
-#     Y_pred = np.where(model.predict(X_test) > 0.5, 1, 0)
-
-#     end = time.time()
-#     predicting_delay_RN.append(end - start)
-
-################################## Nombres d'iterations
-# epochs_range = [30,60, 120]#[10,60,500]
-# max_ep = max(epochs_range)
-#
-# try:
-#     shutil.rmtree('./logs')
-# except:
-#     print("nothing to delete")
-    
-# tensorboard_callback = []
-# for i in range(3):
-#     tensorboard_callback.append(TensorBoard(log_dir="logs\{}".format(i)))#time.time())))
-# # Par invité de commande : 
-# # tensorboard --logdir="./logs" --port 6006
-# cpt = 0
-# for ep in epochs_range:
-#     model = RN_model(layer_sizes, dropout, learning_rate)
-#     #### Apprentissage
-#     start = time.time()
-#     #model.fit(X_train, Y_train, batch_size = 100, epochs = 60)
-#     hist_obj = model.fit(X_train, Y_train, batch_size = batch_size, epochs = ep, validation_data=(X_test, Y_test), callbacks = [tensorboard_callback[cpt]])
-#
-#     end = time.time()
-#     training_delay_RN.append(end - start)
-#
-#     ho_tmp = list(hist_obj.history.values())
-#     ho_tmp = [i + [np.nan for _ in range(max_ep-ep)] for i in ho_tmp ]
-#     history_obj.append(ho_tmp)
-#
-#
-#     #### Prédiction
-#     start = time.time()
-#
-#     Y_pred = np.where(model.predict(X_test) > 0.5, 1, 0)
-#
-#     end = time.time()
-#     predicting_delay_RN.append(end - start)
-
-################################## Learning rate 
-#l_rate_range = [0.00001,0.0005,0.1]
-
-# try:
-#     shutil.rmtree('./logs')
-# except:
-#     print("nothing to delete")
-    
-# tensorboard_callback = []
-# for i in range(3):
-#     tensorboard_callback.append(TensorBoard(log_dir="logs\{}".format(i)))#time.time())))
-# # Par invité de commande : 
-# # tensorboard --logdir="./logs" --port 6006
-# cpt = 0
-# for l_rate in l_rate_range:
-#     model = RN_model(layer_sizes, dropout, l_rate)
-#     #### Apprentissage
-#     start = time.time()
-#     #model.fit(X_train, Y_train, batch_size = 100, epochs = 60)
-#     hist_obj = model.fit(X_train, Y_train, batch_size = batch_size, epochs = epochs, validation_data=(X_test, Y_test), callbacks = [tensorboard_callback[cpt]])
-#
-#     end = time.time()
-#     training_delay_RN.append(end - start)
-#
-#     history_obj.append( list(hist_obj.history.values()))
-#
-#     #### Prédiction
-#     start = time.time()
-#
-#     Y_pred = np.where(model.predict(X_test) > 0.5, 1, 0)
-#
-#     end = time.time()
-#     predicting_delay_RN.append(end - start)
-#     cp+=1
-
-
-# A faire sauf pour epochs
+# Traitement pour affichage
 ho = np.array(history_obj)
-ho = ho.transpose(1,2,0) 
-
-
-sub_title = ['loss','acc','f1','val_loss','val_acc', 'val_f1']
-x_lab = "epochs"
-
-
-leg = [str(i) for i in layer_sizes_range]
-#leg = [str(i) for i in epochs_range]
-#leg = [str(i) for i in l_rate_range]
-
-titre = "RN : HyperParam = number of layer"
-#titre = "RN : HyperParam = layer size"
-#titre = "RN : HyperParam = number of epochs"
-#titre = "RN : HyperParam = learning rate"
+ho = ho.transpose(1,2,0)
+                                                                                                                                      
+leg = [str(i) for i in l_rate_range]                                                                                                                                                
+                                                                                                                                    
+titre = "RN : HyperParam = learning rate"                                                                                                                                           
 
 plot_perf(ho, leg, titre ,sub_title)
 plot_delay(training_delay_RN,predicting_delay_RN,titre)
