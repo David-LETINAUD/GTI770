49bf31d4c34eac812820e27a020e1ed29f56ef0f
thomlioret <lioret.thom@gmail.com>
2019-11-21 11:31:12 -0500

    premier jet : acc/f1 = 20/22%

--- GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/RF_test.py
+++ GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/RF_test.py
@@ -21,6 +21,36 @@ Group :
 GTI770-A19-01
 """
 
-#Imports                                                                                                                                                                             
+#Imports                                                                                                                                                                            
+from functions import *
 
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import KFold
+import sklearn.metrics as metrics
+
+#Ouverture fichier Spectral Derivate
+X,Y = get_data("./tagged_feature_sets/msd-jmirspectral_dev/msd-jmirspectral_dev.csv")
+#print(X[7],Y[7])
+
+rfc = RandomForestClassifier(n_estimators = 100, max_depth = None, n_jobs = 3)
+#n_estimators : nb arbres dans la forêts
+#max_depth : profondeur des arbres - None -> gestion auto
+
+
+scores = []
+accuracy = []
+kf = KFold(n_splits = 5) 
+for train_index, test_index in kf.split(X):
+    #print("TRAIN:", train_index, "TEST:", test_index)
+    X_train, X_test = X[train_index], X[test_index]
+    Y_train, Y_test = Y[train_index], Y[test_index]
+    rfc.fit(X_train,Y_train)
+    Y_pred = rfc.predict(X_test)
+    scores.append(metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None))
+    accuracy.append(metrics.accuracy_score(Y_test, Y_pred))
+
+
+print("moyenne des accuracy :",np.mean(accuracy),"les accuracy sont de : ",accuracy)
+print("moyenne des f1_score :",np.mean(scores),"les f1_scores sont de : ",scores)
+
