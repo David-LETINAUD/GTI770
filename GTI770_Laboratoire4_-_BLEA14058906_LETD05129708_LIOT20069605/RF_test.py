7424ed8da9b5053ef04beccbcaf89e75f415bd6c
thomlioret <lioret.thom@gmail.com>
2019-11-20 19:32:11 -0500

    Création fichiers RF

---
+++ GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/RF_test.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Course :
+        
+GTI770 — Systèmes intelligents et apprentissage machine
+
+Project :
+
+Lab # 4 — Développement d'un système intelligent
+
+Students :
+
+Alexendre Bleau — BLEA14058906
+
+David Létinaud  — LETD05129708                                                                                                                                                                                     
+Thomas Lioret   — LIOT20069605
+
+Group :
+
+GTI770-A19-01
+"""
+
+#Imports                                                                                                                                                                             
+
+import numpy as np
