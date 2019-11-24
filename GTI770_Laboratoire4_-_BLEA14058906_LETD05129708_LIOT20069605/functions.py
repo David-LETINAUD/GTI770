1b7967a1dec3e219f54b94915265a91480b80dc3
David-LETINAUD <david.letinaud@gmail.com>
2019-11-23 23:52:43 -0500

    RN_hyperparam

--- GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/functions.py
+++ GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/functions.py
@@ -21,6 +21,7 @@ import csv
 from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -43,7 +44,7 @@ def get_data(dataset_path):
     Y = le.fit_transform(np.array(features_list.iloc[:,-1]) )   
     X = np.array(features_list.iloc[:,2:-1])
 
-    return X, Y
+    return X, Y, le
 
 
 def plot_perf_epochs(histo,legende,titre,sous_titre): 
@@ -128,3 +129,64 @@ def plot_delay(train_delay,test_delay,titre):
 
     plt.show()
     
+
+
+# Fonction inspirée de : https://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=True,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    #tmp = unique_labels(y_true, y_pred)
+    #classes = classes[tmp]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    #print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label',
+           xlim = (-0.5,len(classes)-0.5),
+           ylim = (-0.5,len(classes)-0.5)
+    )
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    plt.show()
+    return ax
+
+# plot_confusion_matrix(y_test,best_y_pred_SVM,class_names, title="KNN Confusion matrix : K = {}".format(K_range[best_index_KNN]))
