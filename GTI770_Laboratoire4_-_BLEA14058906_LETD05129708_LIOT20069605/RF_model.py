287a9aa2fbb3b6f1d5a43d8a9b9746e3f318125d
thomlioret <lioret.thom@gmail.com>
2019-11-25 11:37:15 -0500

    hyperparam

--- GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/RF_model.py
+++ GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/RF_model.py
@@ -23,7 +23,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import KFold
 import sklearn.metrics as metrics
-
+import time
 
 #Etude des datasets
 
@@ -40,7 +40,7 @@ def RF_dataset_study(path_list,n_estimators = 10,n_splits = 5):
 
 
     output:
-    results (list) : liste contenant le nom du dataset avec les accuracy et f1_score associés.
+    results (ndarray) : liste contenant le nom du dataset avec les accuracy et f1_score associés.
     """
     
     results = []
@@ -67,6 +67,8 @@ def RF_dataset_study(path_list,n_estimators = 10,n_splits = 5):
         m_f1 = np.mean(scores)
         results.append((path,[m_acc,m_f1]))
 
+    results = np.array(results)
+
     return results
 
 
@@ -86,7 +88,7 @@ def RF_nbEstimators(X,Y,list_estimators,n_splits = 5):
     n_splits (int) : nombre de groupe de valeurs pour la cross validation. (default = 5)
     
     output:
-    result (list) : Liste contenant l'accuracy, le f1_score, le temps d'entrainement et de prédiciton.
+    result (ndarray) : Liste contenant l'accuracy, le f1_score, le temps d'entrainement et de prédiciton.
     
     """
 
@@ -119,7 +121,9 @@ def RF_nbEstimators(X,Y,list_estimators,n_splits = 5):
 
         results.append([np.mean(accuracy),np.mean(scores),t_learn,t_pred])
 
-    return results
+    results = np.array(results)
+
+    return results
 
 
 
@@ -135,8 +139,42 @@ def RF_maxDepth(X,Y,list_max_depth,n_splits = 5):
     n_splits (int) : nombre de groupe de valeurs pour la cross validation. (default = 5)
     
     output:
-    result (list) : Liste contenant l'accuracy, le f1_score, le temps d'entrainement et de prédiciton.
+    result (ndarray) : Liste contenant l'accuracy, le f1_score, le temps d'entrainement et de prédiciton.
     """
+
+
+
+    results = []
+    kf = KFold(n_splits)
+    
+    
+    for depth in list_max_depth:
+        
+        rfc = RandomForestClassifier(n_estimators = 10,n_jobs = -1,max_depth = depth)
+        scores = []
+        accuracy = []
+        t1, t2, t3 = 0,0,0
+        t_learn, t_pred = 0,0
+        
+        for train_index, test_index in kf.split(X):
+            t1 = time.time()
+            X_train, X_test = X[train_index], X[test_index]
+            Y_train, Y_test = Y[train_index], Y[test_index]
+            rfc.fit(X_train,Y_train)
+            t2 = time.time()
+            Y_pred = rfc.predict(X_test)
+            t3 = time.time()
+            t_learn += t2 - t1
+            t_pred += t3 - t2
+            scores.append(metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None))
+            accuracy.append(metrics.accuracy_score(Y_test, Y_pred))
+        
+        
+        results.append([np.mean(accuracy),np.mean(scores),t_learn,t_pred])
+
+    results = np.array(results)
+    
+    return results
 
 
 
@@ -153,8 +191,42 @@ def RF_sampleSplit(X,Y,list_min_samples_splits,n_splits = 5):
     n_splits (int) : nombre de groupe de valeurs pour la cross validation. (default = 5)
     
     output:
-    result (list) : Liste contenant l'accuracy, le f1_score, le temps d'entrainement et de prédiciton.
+    result (ndarray) : Liste contenant l'accuracy, le f1_score, le temps d'entrainement et de prédiciton.
     """
+
+
+
+    results = []
+    kf = KFold(n_splits)
+    
+    
+    for mss in list_min_samples_splits:
+        
+        rfc = RandomForestClassifier(n_estimators = 10,n_jobs = -1,max_depth = None, min_samples_split = mss)
+        scores = []
+        accuracy = []
+        t1, t2, t3 = 0,0,0
+        t_learn, t_pred = 0,0
+        
+        for train_index, test_index in kf.split(X):
+            t1 = time.time()
+            X_train, X_test = X[train_index], X[test_index]
+            Y_train, Y_test = Y[train_index], Y[test_index]
+            rfc.fit(X_train,Y_train)
+            t2 = time.time()
+            Y_pred = rfc.predict(X_test)
+            t3 = time.time()
+            t_learn += t2 - t1
+            t_pred += t3 - t2
+            scores.append(metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None))
+            accuracy.append(metrics.accuracy_score(Y_test, Y_pred))
+        
+        
+        results.append([np.mean(accuracy),np.mean(scores),t_learn,t_pred])
+
+    results = np.array(results)
+
+    return results
 
 
 def RF_sampleLeaf(X,Y,list_min_samples_leaf,n_splits = 5):
@@ -170,17 +242,90 @@ def RF_sampleLeaf(X,Y,list_min_samples_leaf,n_splits = 5):
     n_splits (int) : nombre de groupe de valeurs pour la cross validation. (default = 5)
     
     output:
-    result (list) : Liste contenant l'accuracy, le f1_score, le temps d'entrainement et de prédiciton.
+    result (ndarray) : Liste contenant l'accuracy, le f1_score, le temps d'entrainement et de prédiciton.
     """
+
+    results = []
+    kf = KFold(n_splits)
     
     
-    
+    for msl in list_min_samples_leaf:
+        
+        rfc = RandomForestClassifier(n_estimators = 10,n_jobs = -1,max_depth = None, min_samples_leaf = msl)
+        scores = []
+        accuracy = []
+        t1, t2, t3 = 0,0,0
+        t_learn, t_pred = 0,0
+        
+        for train_index, test_index in kf.split(X):
+            t1 = time.time()
+            X_train, X_test = X[train_index], X[test_index]
+            Y_train, Y_test = Y[train_index], Y[test_index]
+            rfc.fit(X_train,Y_train)
+            t2 = time.time()
+            Y_pred = rfc.predict(X_test)
+            t3 = time.time()
+            t_learn += t2 - t1
+            t_pred += t3 - t2
+            scores.append(metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None))
+            accuracy.append(metrics.accuracy_score(Y_test, Y_pred))
+        
+        
+        results.append([np.mean(accuracy),np.mean(scores),t_learn,t_pred])
+
+    results = np.array(results)
+
+    return results
+
+
 def RF_Kfold_Split(X,Y,list_nb_Ksplit):
 
     """
-    Fonction permettant de déterminer le meilleur apprentissage en terme de k-split
-    """
+    Fonction permettant de déterminer le meilleur apprentissage en terme de k-split.
     
+    input:
+    X (ndarray) : tableau des primitives
+    Y (ndarray) : tableau des étiquettes
+    list_nb_Ksplit (list) : liste contenant le nombre de d'ensemble à tester.
+    
+    output:
+    result (ndarray) : Liste contenant l'accuracy, le f1_score, le temps d'entrainement et de prédiciton.
+    """
+
+
+    results = []
+    
+    
+    
+    for ns in list_nb_Ksplit:
+
+        kf = KFold(ns)
+        
+        rfc = RandomForestClassifier(n_estimators = 10,n_jobs = -1,max_depth = None)
+        scores = []
+        accuracy = []
+        t1, t2, t3 = 0,0,0
+        t_learn, t_pred = 0,0
+        
+        for train_index, test_index in kf.split(X):
+            t1 = time.time()
+            X_train, X_test = X[train_index], X[test_index]
+            Y_train, Y_test = Y[train_index], Y[test_index]
+            rfc.fit(X_train,Y_train)
+            t2 = time.time()
+            Y_pred = rfc.predict(X_test)
+            t3 = time.time()
+            t_learn += t2 - t1
+            t_pred += t3 - t2
+            scores.append(metrics.f1_score(Y_test, Y_pred, labels=None, pos_label=1, average="weighted", sample_weight=None))
+            accuracy.append(metrics.accuracy_score(Y_test, Y_pred))
+        
+        
+        results.append([np.mean(accuracy),np.mean(scores),t_learn,t_pred])
+
+    results = np.array(results)
+    
+    return results
     
     
     
