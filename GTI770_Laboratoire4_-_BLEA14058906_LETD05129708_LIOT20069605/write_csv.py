6b5d56d3b4e026f7e22c4611c7957b6ee5fe59ea
thomlioret <lioret.thom@gmail.com>
2019-12-05 18:06:10 -0500

    creation fichier writecsv

---
+++ GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/write_csv.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+    Course :
+    
+    GTI770 — Systèmes intelligents et apprentissage machine
+    
+    Project :
+    
+    Lab # 4 — Développement d'un système intelligent
+    
+    Students :
+    
+    Alexendre Bleau — BLEA14058906
+    
+    David Létinaud  — LETD05129708
+    Thomas Lioret   — LIOT20069605
+    
+    Group :
+    
+    GTI770-A19-01
+    """
+
+#Imports
+from functions import *
+from RF_model import *
+import numpy as np
+import csv
+
+#write file
