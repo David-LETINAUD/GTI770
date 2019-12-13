0e4ce91375162da319ee1d18536cbf0474599ab3
David-LETINAUD <david.letinaud@gmail.com>
2019-12-13 16:20:22 -0500

    combining 6 modeles

--- GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/RN_make_models.py
+++ GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/RN_make_models.py
@@ -35,8 +35,8 @@ tf.disable_v2_behavior()
 
 # The others
 dataset_path_tab = []
-dataset_path_tab.append("./tagged_feature_sets/msd-ssd_dev/msd-ssd_dev.csv") # best =>31%
-dataset_path_tab.append("./tagged_feature_sets/msd-jmirmfccs_dev/msd-jmirmfccs_dev.csv")
+# dataset_path_tab.append("./tagged_feature_sets/msd-ssd_dev/msd-ssd_dev.csv") # best =>31%
+# dataset_path_tab.append("./tagged_feature_sets/msd-jmirmfccs_dev/msd-jmirmfccs_dev.csv")
 dataset_path_tab.append("./tagged_feature_sets/msd-marsyas_dev_new/msd-marsyas_dev_new.csv") # 2nd => 27%
 
 layer_sizes = [500]
@@ -73,8 +73,8 @@ for i in range(len(dataset_path_tab)):
 
 # Save keras model
 checkpoint_path = []
-checkpoint_path.append("Models/MLP_model_SSD/cp.ckpt")
-checkpoint_path.append("Models/MLP_model_MFCC/cp.ckpt")
+# checkpoint_path.append("Models/MLP_model_SSD/cp.ckpt")
+# checkpoint_path.append("Models/MLP_model_MFCC/cp.ckpt")
 checkpoint_path.append("Models/MLP_model_MARSYAS/cp.ckpt")
 
 checkpoint_dir = []
@@ -86,7 +86,7 @@ for cp in checkpoint_path:
                                                     save_weights_only=True,
                                                     verbose=1))
 
-cpt = 0
+# cpt = 0
 for path_ in dataset_path_tab:
     X, Y, id, le = get_data(path_)
     class_names = list(le.classes_)
@@ -103,40 +103,40 @@ for path_ in dataset_path_tab:
     nb_classes = max(Y)+1
     train_size = len(X)
 
-    model = RN_model(layer_sizes, dropout, learning_rate, nb_features, nb_classes)
-    #### Apprentissage                                                                                                                                                               
-    start = time.time()                                                                                                                   
-    hist_obj = model.fit(X_train[0:train_size], Y_train[0:train_size], batch_size = batch_size, epochs = epochs, validation_data=(X_test, Y_test), callbacks = [tensorboard_callback[cpt],cp_callback[cpt]], class_weight=class_weights) 
-    end = time.time()
+#     model = RN_model(layer_sizes, dropout, learning_rate, nb_features, nb_classes)
+#     #### Apprentissage                                                                                                                                                               
+#     start = time.time()                                                                                                                   
+#     hist_obj = model.fit(X_train[0:train_size], Y_train[0:train_size], batch_size = batch_size, epochs = epochs, validation_data=(X_test, Y_test), callbacks = [tensorboard_callback[cpt],cp_callback[cpt]], class_weight=class_weights) 
+#     end = time.time()
 
-    training_delay_RN.append(end - start)
-    history_obj.append( list(hist_obj.history.values()))
+#     training_delay_RN.append(end - start)
+#     history_obj.append( list(hist_obj.history.values()))
 
-    #### Prédiction                                                                                                                                                                  
-    start = time.time()
-    Y_pred_temp = model.predict(X_test)
-    end = time.time()
-    predicting_delay_RN.append(end - start)
+#     #### Prédiction                                                                                                                                                                  
+#     start = time.time()
+#     Y_pred_temp = model.predict(X_test)
+#     end = time.time()
+#     predicting_delay_RN.append(end - start)
 
-    # remise en forme de Y_pred
-    Y_pred = []
-    for i in Y_pred_temp:
-        Y_pred.append(np.argmax(i))    
+#     # remise en forme de Y_pred
+#     Y_pred = []
+#     for i in Y_pred_temp:
+#         Y_pred.append(np.argmax(i))    
     
-    f1 = metrics.f1_score(Y_test, Y_pred,average='weighted')
-    acc = metrics.accuracy_score(Y_test, Y_pred)
-    print("acc :", acc,"f1 :", f1)
-    #  Assurez-vous d’avoir le paramètre average=‘weighted’ afin de pondérer correctement le score en fonction du nombre d’instances de chaque classe.
-    f1_RN.append(f1)
-    acc_RN.append(acc)
-    cpt+=1
+#     f1 = metrics.f1_score(Y_test, Y_pred,average='weighted')
+#     acc = metrics.accuracy_score(Y_test, Y_pred)
+#     print("acc :", acc,"f1 :", f1)
+#     #  Assurez-vous d’avoir le paramètre average=‘weighted’ afin de pondérer correctement le score en fonction du nombre d’instances de chaque classe.
+#     f1_RN.append(f1)
+#     acc_RN.append(acc)
+#     cpt+=1
 
 #### Prédiction            
 # Loads the weights
 print("NEW MODEL")
 model2 = RN_model(layer_sizes, dropout, learning_rate, nb_features, nb_classes)
 print(checkpoint_path)
-model2.load_weights(checkpoint_path[2])
+model2.load_weights(checkpoint_path[0])
 
 # Re-evaluate the model
 #loss,acc = model.evaluate(test_images,  test_labels, verbose=2)
---
+++ GTI770_Laboratoire4_-_BLEA14058906_LETD05129708_LIOT20069605/combining_v2.py
@@ -0,0 +1,208 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Course :
+GTI770 — Systèmes intelligents et apprentissage machine
+
+Project :
+Lab # 4 - Développement d’un système intelligent
+
+Students :
+Alexendre Bleau — BLEA14058906
+David Létinaud  — LETD05129708
+Thomas Lioret   — LIOT20069605
+
+Group :
+GTI770-A19-01
+"""
+
+# inspiré de : https://www.python-course.eu/Boosting.php
+from functions import *
+from sklearn.utils import class_weight
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from RN_model import RN_model
+import pickle
+from sklearn.decomposition import PCA
+from SVC_model import PCA_Find_ncomp
+
+from sklearn import metrics
+
+layer_sizes = [500] # OK
+epochs = 100 # OK avec 100
+learning_rate = 0.001
+batch_size = 500
+
+dropout = 0.5
+
+
+
+def combining(data_path, weights, RN_path, RF_path, SVM_path, SVM_N_comp, with_labels = True ):
+    if with_labels == True:
+        X, Y, id, le = get_data(data_path)
+    else :
+        X, id, classes_ = get_data_whithout_labels(data_path)
+
+    # dataset_size = 100 #len(X)
+    # X = X[:dataset_size]
+    # if with_labels == True:
+    #     Y = Y[:dataset_size]
+
+    X = preprocessing.normalize(X, norm='max',axis = 0)
+
+    # PCA pour SVM (si SVM_N_comp<0 => PCA non utilisé )
+    if SVM_N_comp>0:
+        pca = PCA(n_components=SVM_N_comp)
+        pca.fit(X)
+        PCA_X = pca.transform(X)
+    else:
+        PCA_X = X
+
+    # Calcul nb de features et de classes
+    nb_features = len(X[0])
+    if with_labels == True:
+        nb_classes = max(Y)+1
+    else :
+        nb_classes = len(classes_)
+
+    # LOAD modeles
+    RN_model_ = RN_model(layer_sizes, dropout, learning_rate, nb_features, nb_classes)
+    RN_model_.load_weights(RN_path)
+
+    pickle_in = open(RF_path, "rb")
+    RF_model_ = pickle.load(pickle_in)
+
+    pickle_in = open(SVM_path, "rb")
+    SVM_model_ = pickle.load(pickle_in)
+
+    #########################   predict modele
+    # RN MODEL
+    Y_pred_RN = RN_model_.predict_proba(X)
+
+    # RF MODEL
+    Y_pred_RF = RF_model_.predict_proba(X)
+
+    #SVM MODEL
+    Y_pred_SVM = SVM_model_.predict_proba(PCA_X)
+
+    ######################### Combinaison des décisions
+    Y_pred_one_hot = weights[0] * Y_pred_RN + weights[1] * Y_pred_RF + weights[2]*Y_pred_SVM 
+    
+    return Y_pred_one_hot, id
+    
+
+
+def run_combining(data_path_tab, weights_tab, RN_path, RF_path, SVM_path,SVM_N_comp_tab, with_labels = True):
+    classes_ = ['BIG_BAND','BLUES_CONTEMPORARY','COUNTRY_TRADITIONAL','DANCE',
+                'ELECTRONICA','EXPERIMENTAL','FOLK_INTERNATIONAL','GOSPEL','GRUNGE_EMO',
+                'HIP_HOP_RAP','JAZZ_CLASSIC','METAL_ALTERNATIVE','METAL_DEATH',
+                'METAL_HEAVY','POP_CONTEMPORARY','POP_INDIE','POP_LATIN','PUNK','REGGAE',
+                'RNB_SOUL','ROCK_ALTERNATIVE','ROCK_COLLEGE','ROCK_CONTEMPORARY',
+                'ROCK_HARD','ROCK_NEO_PSYCHEDELIA']
+
+        # SSD MFCC MARSYAS
+    # ACC = [0.274,0.155, 0.238]        # Accuracy correspondant à la combinaison de 3 modèles RN RF et SVM par features set
+    Ponderation = [0.666,0.402,0.599]   # Somme des accuracys de chaque modèles en réponse au dataset SSD MFCC MARSYAS
+    ACC = [0.274,0, 0.238]                
+    weight_L2 = [p*a for a,p in zip(ACC,Ponderation)]
+    sum_L2 = np.sum(np.array(weight_L2))
+    weight_L2_normalize = [w/sum_L2 for w in weight_L2]
+
+    id_genre_pred = []
+    perf = []
+
+    Y_pred_one_hot_tab=[]
+
+    cpt = 0
+    for data_path,weights,rn_p,rf_p,svm_p, n_comp in zip(data_path_tab,weights_tab, RN_path, RF_path, SVM_path,SVM_N_comp_tab):
+        if cpt != 1:
+            r,id = combining(data_path,weights,rn_p,rf_p,svm_p,n_comp, with_labels)
+            Y_pred_one_hot_tab.append(r)
+        else :
+            Y_pred_one_hot_tab.append(0)
+        cpt+=1
+
+        # Les 3 résultats doivent êux même être combiné
+
+        # id_genre_pred.append(r[0:2])
+        # if with_labels == True:
+        #     perf.append(r[2:4])
+    #Y_pred_one_hot = weight_L2[0]*Y_pred_one_hot_tab[0] + weight_L2[1]*Y_pred_one_hot_tab[1]+ weight_L2[2]*Y_pred_one_hot_tab[2]
+    #Y_pred_one_hot = weight_L2_normalize[0]*Y_pred_one_hot_tab[0] + weight_L2_normalize[1]*Y_pred_one_hot_tab[1] + weight_L2_normalize[2]*Y_pred_one_hot_tab[2]
+    Y_pred_one_hot = weight_L2_normalize[0]*Y_pred_one_hot_tab[0] + weight_L2_normalize[2]*Y_pred_one_hot_tab[2]
+    Y_pred = []
+
+    for i in Y_pred_one_hot:
+        Y_pred.append(np.argmax(i))
+
+    Y_pred_label = [classes_[i] for i in Y_pred ]
+
+    return id, Y_pred_label
+    # if with_labels == True:
+    #     return id_genre_pred, perf
+    # else :
+    #      return id_genre_pred
+
+
+#data_path = ["./tagged_feature_sets/msd-ssd_dev/msd-ssd_dev.csv", "./tagged_feature_sets/msd-jmirmfccs_dev/msd-jmirmfccs_dev.csv", "./tagged_feature_sets/msd-marsyas_dev_new/msd-marsyas_dev_new.csv"] #=> MLP 30.7%
+data_path_nolabels = ["./untagged_feature_sets/msd-ssd_test_nolabels/msd-ssd_test_nolabels.csv", "./untagged_feature_sets/msd-jmirmfccs_test_nolabels/msd-jmirmfccs_test_nolabels.csv", "./untagged_feature_sets/msd-marsyas_test_new_nolabels/msd-marsyas_test_new_nolabels.csv"] #=> MLP 30.7%
+
+# Calculer les poids
+#           RN    RF  SVM    
+MSSD_acc = [0.273, 0.21, 0.183]
+MFCC_acc = [0.155,0.13,0.117]
+MARSYAS_acc = [0.224,0.208,0.167]
+
+# RN_acc = [0.353,0.249, 0.320]
+# RN_f1 = [0.333,0.220,0.299]
+weight = []
+# Le poids est calculé selon le pourcentage que représente l'accuracy..
+# .. du modèle sur la somme total des accuracy sur le dataset étudié
+MSSD_total = np.sum(np.array(MSSD_acc))
+weight.append([a/MSSD_total for a in MSSD_acc])
+MFCC_total = np.sum(np.array(MFCC_acc))
+weight.append([a/MFCC_total for a in MFCC_acc])
+MARSYAS_total = np.sum(np.array(MARSYAS_acc))
+weight.append([a/MARSYAS_total for a in MARSYAS_acc])
+print(weight)
+#weight = [[0.4,0.2,0.4], [0.4,0.55,0.05], [0.35,0.3,0.35]]
+
+
+RN_models_path = ["Models/MLP_model_SSD/cp.ckpt", "Models/MLP_model_MFCC/cp.ckpt", "Models/MLP_model_MARSYAS/cp.ckpt" ]
+RF_models_path = ["./Models/rfc_ssd.sav","./Models/rfc_mfcc.sav","./Models/rfc_marsyas.sav"]
+SVM_models_path = ["./Models/svm_ssd.sav","./Models/svm_mfcc.sav","./Models/svm_marsyas.sav"]
+SVM_N_comp_tab = [28, -1,32]
+
+#run_combining(data_path,weight,RN_models_path, RF_models_path, SVM_models_path,SVM_N_comp_tab,with_labels=True)
+
+pred = run_combining(data_path_nolabels,weight,RN_models_path, RF_models_path, SVM_models_path,SVM_N_comp_tab,with_labels=False)
+
+
+
+#pred_X, pred_Y, pred_Z = pred[0][:][:],pred[1][:][:],pred[2][:][:]
+
+
+# list_files = ['SSD_pred_file.csv','MFCC_pred_file.csv','MARSYAS_pred_file.csv']
+
+def write_pred_csv(title_csv,prediction_list):
+    
+    """
+        ecriture csv, ! format prediction_list,
+        """
+    
+    with open(title_csv, mode = 'w') as pred_file :
+        file_writer = csv.writer(pred_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        file_writer.writerow(['id','genre'])
+        for i in range(len(prediction_list[0])):
+            file_writer.writerow([prediction_list[0][i],prediction_list[1][i]])
+
+
+write_pred_csv("6_modeles.csv",pred)
+# write_pred_csv(list_files[1],pred_Y)
+# write_pred_csv(list_files[2],pred_Z)
+
+
+#Final SSD_acc = 0.26981 -> 0.404
+#Final MFCC_acc = 0.15386 -> 0.231
+#Final Marsyas_acc = 0.24379 -> 0.365
